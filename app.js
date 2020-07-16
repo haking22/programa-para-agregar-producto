31bca005af223d7b25cc04395e12f5241666815e
Luis <luis.mrodriguez06@gmail.com>
2020-07-15 21:13:48 -0400

    version 3 minuto 51.54

--- app.js
+++ app.js
@@ -20,12 +20,33 @@ tendremos otra clase que duncionara de interfas que tendra unos metodo
  */
 
  class UI{
-     addProduct(){
+     addProduct(product){
+        const productList= document.getElementById('product-list');
+        const element = document.createElement('div');
+        element.innerHTML = `
+            <div class= 'card text-center mb-4'>
+                <div class= 'card-body'>
+                    <strong>Nombre del Producto</strong>: ${product.name}
+                    <strong>Precio del Producto </strong>: ${product.price}
+                    <strong>Año del Producto</strong>: ${product.year}
+                    <a href='#' class= 'btn btn-danger' name='delete'>Borrar</a>
+                </div>
 
+            </div>
+        `;
+        productList.appendChild(element);
+     }
+     resetForm(){
+         document.getElementById('product-form').reset();
+         
      }
 
-     deleteProduct(){
-
+     deleteProduct(element){
+         //para eliminar un elemento
+        if(element.name==='delete'){
+            // buscamos el div completo y lo eliminamos
+            element.parentElement.parentElement.parentElement.remove();
+        }
      }
 
      showMessage(){
@@ -33,7 +54,7 @@ tendremos otra clase que duncionara de interfas que tendra unos metodo
      }
  }
 
- // evento del DOM
+ // evento del DOM adactar
 document.getElementById('product-form')
     .addEventListener('submit', function(e) {
         // almacenado en una costante los evento
@@ -41,9 +62,19 @@ document.getElementById('product-form')
         const price = document.getElementById('price').value;
         const year = document.getElementById('year').value;
         console.log(name,price,year);
-        // mostrando los elemento y creando el objeto producto
-        console.log(new Product (name,price,year));
+        //los elemento y creando el objeto producto
+        const product = new Product (name,price,year);
+        //costando para agregar el producto
+        const ui= new UI();
+        ui.addProduct(product);
+        // llamando el metodo de reseteo del formulario
+        ui.resetForm();
+        //
         e.preventDefault();
-
-
 });
+// evento del para eliminar
+document.getElementById('product-list').addEventListener('click', function(e){
+    const ui = new UI();
+    ui.deleteProduct(e.target);
+});
+
